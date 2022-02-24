a2f153e8eb63724923f12830ec3f17427bf427b6
satilasha <swatighosh2111@gmail.com>
2022-02-24 17:09:22 +0530

    query param

--- src/routes/route.js
+++ src/routes/route.js
@@ -2,94 +2,54 @@ const express = require('express');
 const router = express.Router();
 
 
-let players =  [
-    {
-        "name": "manish",
-        "dob": "1/1/1995",
-        "gender": "male",
-        "city": "jalandhar",
-        "sports": [
-            "swimming"
-        ],
-        "bookings": [
-            {
-                "bookingNumber": 1,
-                "sportId": "",
-                "centerId": "",
-                "type": "private",
-                "slot": '16286598000000',
-                "bookedOn": '31/08/2021',
-                "bookedFor": '01/09/2021'
-            },
-            {
-                "bookingNumber": 2,
-                "sportId": "",
-                "centerId": "",
-                "type": "private",
-                "slot": '16286518000000',
-                "bookedOn": '31/08/2001',
-                "bookedFor": '01/09/2001'
-            },
-        ]
-    },
-    {
-        "name": "gopal",
-        "dob": "1/09/1995",
-        "gender": "male",
-        "city": "delhi",
-        "sports": [
-            "soccer"
-        ],
-        "bookings": []
-    },
-    {
-        "name": "lokesh",
-        "dob": "1/1/1990",
-        "gender": "male",
-        "city": "mumbai",
-        "sports": [
-            "soccer"
-        ],
-        "bookings": []
-    },
+
+
+// ASSIGNMENT:
+// you will be given an array of persons ( i.e an array of objects )..each person will have a {name: String , age: Number, votingStatus: true/false(Boolean)}
+// take input in query param as votingAge..and for all the people above that age, change votingStatus as true
+// also return an array consisting of only the person that can vote
+ 
+//  take this as sample for array of persons:
+let persons= [
+ {
+ name: "PK",
+ age: 10,
+ votingStatus: false
+},
+{
+ name: "SK",
+ age: 20,
+ votingStatus: false
+},
+{
+ name: "AA",
+ age: 70,
+ votingStatus: false
+},
+{
+ name: "SC",
+ age: 5,
+ votingStatus: false
+},
+{
+ name: "HO",
+ age: 40,
+ votingStatus: false
+}
 ]
 
-router.post("/player", function(req, res) {
-    let ele = req.body.name
-    const index = players.findIndex(object => object.name === ele)
-       if(index === -1){
-      players.push(req.body)
-        }
-        else{
-            res.send("player exists")
-        }
-    res.send(  { players }  )
-       
-})
 
-router.post("/players/:playerName/bookings/:bookingId", function(req, res) {
-    let play = req.params.playerName
-    let book =req.params.bookingId
-    
-    const index = players.findIndex(object => object.name === play)
-    if(index === -1){
-        res.send('player does not exists')   
-    }
 
-    else{
-    let a = players[index].bookings
-    const i = a.findIndex(object => object.bookingNumber == book)
-        if(i === -1 ){
-            a.push(req.body)
-            res.send(  players[index] )
-           
-        }
-        else{
-            res.send("booking exists")
+router.post("/person", function(req, res) {
+    let VotingAge = req.query.VotingAge
+    for(i = 0;i<persons.length;i++){
+        if(persons[i].age>=VotingAge){
+            persons[i].votingStatus = true
         }
     }
-
-       
+   
+    res.send(  { persons }  )
 })
+
 
 module.exports = router;
